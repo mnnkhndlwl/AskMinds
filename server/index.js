7057b1fca5439b88b9735165f1ec28a486c089b5
Anup <106077220+anup2903@users.noreply.github.com>
2024-03-20 00:47:54 +0530

    dependency changes

--- server/index.js
+++ server/index.js
@@ -52,10 +52,10 @@ app.use('/teacher', teacherRoutes);
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "./client/dist")));
+  app.use(express.static(path.join(__dirname1, "../client/dist")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname1, "./client/dist", "index.html"))
+    res.sendFile(path.join(__dirname1, "../client/dist", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
